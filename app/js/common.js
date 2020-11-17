ebf7e8633ac02d3410e30a97a9bb5cfb2d8eac1c
dpodbereznyh <dpodbereznyh@gmail.com>
2020-11-17 12:55:30 +0500

    Цели. Время трансляции

--- app/js/common.js
+++ app/js/common.js
@@ -30,5 +30,55 @@ $(document).ready(function () {
         },
     });
   });
+  //Метрика
+  var ymID = 69364525;
+
+  var metrikaGoals = function () {
+
+
+      $('.js-onClickGoal').click(function () {
+
+          var goal = this.dataset.goal;
+          // var gtagGoal = this.dataset.gtagGoal;
+          // var thisGoal = getCookie(goal);
+          if (typeof thisGoal == "undefined") {
+              if ((typeof goal !== 'undefined' || goal != '') && typeof ym !== 'undefined') {
+                  ym(ymID, 'reachGoal', goal);
+              }
+
+              var date = new Date(new Date().getTime() + 720 * 3600 * 1000);
+              document.cookie = goal + "=1; path=/; expires=" + date.toUTCString();
+          }
+
+
+      });
+
+      $('.js-validate-form').click(function () {
+          var form = $(this).closest('.js-form-default');
+          var validated = 1;
+
+          $(form).find('input[required]').each(function (indexInArray, valueOfElement) {
+              if ($(valueOfElement).val() == "") {
+                  validated = 0;
+              }
+          });
+
+          if (validated == 0) {
+
+
+              var goal = this.dataset.goal;
+              // var gtagGoal = this.dataset.gtagGoal;
+
+              if ((typeof goal !== 'undefined' || goal != '') && typeof ym !== 'undefined') {
+                  ym(ymID, 'reachGoal', goal);
+              }
+          }
+          console.log(form);
+      });
+  };
+
+  $(document).ready(function () {
+      metrikaGoals();
+  });
 });
 
